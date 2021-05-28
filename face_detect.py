504aec215dbb9edd208893ccfb844cb75df1f976
Chauhan-Hemant <56783033+Chauhan-Hemant@users.noreply.github.com>
2021-05-28 11:13:57 +0530

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plot
 
 # Load the cascade
+"pre-trained model for frontal face detection from the OpenCV"
 haar_cascade_face = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 # Read the input image
@@ -13,14 +14,17 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Detect faces
 all_faces = haar_cascade_face.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
 
-# Let us print the no. of faces found
+# print the no. of faces found
 print('Faces found: ', len(all_faces))
 
+# drawing the rectangle over the face to clearly pointing it out in the image
 for (x, y, w, h) in all_faces:
     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-# output
+# Display the image
 cv2.imshow('image', image)
 plot.imshow(all_faces)
+
+# for exiting the program
 print("Press any key in the image to exit.")
 cv2.waitKey()
